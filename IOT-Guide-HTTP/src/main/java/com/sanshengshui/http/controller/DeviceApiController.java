d7e899d1177da3a00bc54b18bbc821e739b50da1
sanshengshui <lovewsic@gmail.com>
2019-04-03 14:38:42 +0800

    :construction: 遥测上传API

--- IOT-Guide-HTTP/src/main/java/com/sanshengshui/http/controller/DeviceApiController.java
+++ IOT-Guide-HTTP/src/main/java/com/sanshengshui/http/controller/DeviceApiController.java
@@ -3,12 +3,15 @@ package com.sanshengshui.http.controller;
 import com.google.gson.JsonParser;
 import com.sanshengshui.tsl.adaptor.JsonConverter;
 import com.sanshengshui.tsl.data.kv.AttributeKvEntry;
+import com.sanshengshui.tsl.data.kv.KvEntry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.async.DeferredResult;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 
@@ -31,6 +34,21 @@ public class DeviceApiController {
         Set<AttributeKvEntry> attributeKvEntrySet = JsonConverter.convertToAttributes(new JsonParser().parse(json)).getAttributes();
         for (AttributeKvEntry attributeKvEntry : attributeKvEntrySet){
             System.out.println("属性名="+attributeKvEntry.getKey()+" 属性值="+attributeKvEntry.getValueAsString());
+        }
+        return responseWriter;
+    }
+
+    @RequestMapping(value = "/{deviceToken}/telemetry",method = RequestMethod.POST)
+    public DeferredResult<ResponseEntity> postTelemetry(@PathVariable("deviceToken") String deviceToken,
+                                                        @RequestBody String json){
+        DeferredResult<ResponseEntity> responseWriter = new DeferredResult<ResponseEntity>();
+        responseWriter.setResult(new ResponseEntity(HttpStatus.ACCEPTED));
+        Map<Long, List<KvEntry>> telemetryMaps = JsonConverter.convertToTelemetry(new JsonParser().parse(json)).getData();
+        for (Map.Entry<Long,List<KvEntry>> entry : telemetryMaps.entrySet()) {
+            System.out.println("key= " + entry.getKey());
+            for (KvEntry kvEntry: entry.getValue()) {
+                System.out.println("属性名="+kvEntry.getKey()+ " 属性值="+kvEntry.getValueAsString());
+            }
         }
         return responseWriter;
     }
