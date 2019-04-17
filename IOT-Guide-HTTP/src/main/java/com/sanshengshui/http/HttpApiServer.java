70eed4d2db76cf00c09780be92cb56c35cf13877
sanshengshui <lovewsic@gmail.com>
2019-04-17 09:23:42 +0800

    :ok_hand: update http api

--- IOT-Guide-HTTP/src/main/java/com/sanshengshui/http/HttpApiServer.java
+++ IOT-Guide-HTTP/src/main/java/com/sanshengshui/http/HttpApiServer.java
@@ -1,17 +1,20 @@
+package com.sanshengshui.http;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @Author: 穆书伟
- * @Date: 19-4-2 下午3:45
+ * @Date: 19-4-17 上午9:17
  * @Version 1.0
  */
 @SpringBootApplication(scanBasePackages = {"com.sanshengshui.http"})
-public class IOTHttpServer {
+public class HttpApiServer {
+
     /**
      * @param args
      */
     public static void main(String[] args) {
-        SpringApplication.run(IOTHttpServer.class);
+        SpringApplication.run(HttpApiServer.class);
     }
 }
