283a60284311867448faf141b4cca166f6aaf714
mushuwei <lovewsic@gmail.com>
2020-06-10 18:05:54 +0800

    :heavy_plus_sign: add spring-boot-starter-websocket dependency

--- IOT-Guide-RealTime/src/main/java/iot/technology/realtime/RealTimeServer.java
+++ IOT-Guide-RealTime/src/main/java/iot/technology/realtime/RealTimeServer.java
@@ -1,12 +1,16 @@
 package iot.technology.realtime;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author james mu
  * @date 2020/5/20 17:08
  */
+@SpringBootApplication(scanBasePackages = {"iot.technology.realtime"})
 public class RealTimeServer {
 
     public static void main(String[] args) {
-        System.out.println("waiting update......!");
+        SpringApplication.run(RealTimeServer.class);
     }
 }
