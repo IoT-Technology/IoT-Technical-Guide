75cdffa160305ff3e77d3318ded9b52501fbb609
mushuwei <lovewsic@gmail.com>
2020-06-30 15:06:56 +0800

    :sparkles: add gateway OpcUA features

---
+++ IOT-Guide-Gateway-OPC(UA)/src/main/java/iot/technology/client/OpcUAServer.java
@@ -0,0 +1,12 @@
+package iot.technology.client;
+
+/**
+ * @author james mu
+ * @date 2020/6/30 14:54
+ */
+public class OpcUAServer {
+
+    public static void main(String[] args) {
+        System.out.println("waiting update......!");
+    }
+}
---
+++ IOT-Guide-Gateway-OPC(UA)/src/main/java/iot/technology/server/OpcUAClient.java
@@ -0,0 +1,12 @@
+package iot.technology.server;
+
+/**
+ * @author james mu
+ * @date 2020/6/30 14:54
+ */
+public class OpcUAClient {
+
+    public static void main(String[] args) {
+        System.out.println("waiting update......!");
+    }
+}
