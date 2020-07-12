0cfc4899a86ba409bc785b75402782005ab32e25
sanshengshui <lovewsic@gmail.com>
2020-07-12 23:10:01 +0800

    :sparkles: add protobuf module

---
+++ IOT-Guide-Protobuf/src/main/java/iot/technology/protobuf/ProtobufServer.java
@@ -0,0 +1,12 @@
+package iot.technology.protobuf;
+
+/**
+ * @author james mu
+ * @date 2020/7/12 23:04
+ */
+public class ProtobufServer {
+
+    public static void main(String[] args) {
+        System.out.println("waiting update......!");
+    }
+}
