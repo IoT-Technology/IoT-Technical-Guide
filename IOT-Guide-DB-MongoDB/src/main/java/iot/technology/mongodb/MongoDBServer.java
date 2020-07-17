a1fc06ca40bc989ab977360d850f7b711e9bc358
sanshengshui <lovewsic@gmail.com>
2020-07-17 22:40:48 +0800

    :sparkles: add mongodb and postgresql module

---
+++ IOT-Guide-DB-MongoDB/src/main/java/iot/technology/mongodb/MongoDBServer.java
@@ -0,0 +1,11 @@
+package iot.technology.mongodb;
+
+/**
+ * @author james mu
+ * @date 2020/7/17 22:36
+ */
+public class MongoDBServer {
+    public static void main(String[] args) {
+        System.out.println("waiting update......!");
+    }
+}
---
+++ IOT-Guide-DB-PostgreSQL/src/main/java/iot/technology/postgresql/PostgreSQLServer.java
@@ -0,0 +1,11 @@
+package iot.technology.postgresql;
+
+/**
+ * @author james mu
+ * @date 2020/7/17 22:36
+ */
+public class PostgreSQLServer {
+    public static void main(String[] args) {
+        System.out.println("waiting update......!");
+    }
+}
