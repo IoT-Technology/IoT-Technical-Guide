db87eb145c33fd4763533d1830324e4400938580
mushuwei <lovewsic@gmail.com>
2020-07-23 18:16:13 +0800

    :beers: deal with websocket module

---
+++ IOT-Guide-RealTime/src/main/java/iot/technology/realtime/ChatObject.java
@@ -0,0 +1,35 @@
+package iot.technology.realtime;
+
+
+/**
+ * @author james mu
+ * @date 2020/7/23 17:34
+ */
+public class ChatObject {
+
+    private String userName;
+    private String message;
+
+    public ChatObject() {
+    }
+
+    public ChatObject(String userName, String message) {
+        super();
+        this.userName = userName;
+        this.message = message;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
