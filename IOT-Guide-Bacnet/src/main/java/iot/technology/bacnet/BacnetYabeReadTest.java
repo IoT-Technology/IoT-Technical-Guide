0e6e60539874d3990dd953e32298cf2050611b15
sanshengshui <lovewsic@gmail.com>
2021-10-25 23:23:27 +0800

    :construction: add BACnet read test

--- IOT-Guide-Bacnet/src/main/java/iot/technology/bacnet/BacnetYabeReadTest.java
+++ IOT-Guide-Bacnet/src/main/java/iot/technology/bacnet/BacnetYabeReadTest.java
@@ -4,25 +4,69 @@ import com.serotonin.bacnet4j.LocalDevice;
 import com.serotonin.bacnet4j.RemoteDevice;
 import com.serotonin.bacnet4j.exception.BACnetException;
 import com.serotonin.bacnet4j.npdu.ip.IpNetwork;
+import com.serotonin.bacnet4j.type.Encodable;
+import com.serotonin.bacnet4j.type.enumerated.ObjectType;
+import com.serotonin.bacnet4j.type.enumerated.PropertyIdentifier;
 import com.serotonin.bacnet4j.type.primitive.ObjectIdentifier;
+import com.serotonin.bacnet4j.type.primitive.UnsignedInteger;
+import com.serotonin.bacnet4j.util.PropertyValues;
+import com.serotonin.bacnet4j.util.ReadListener;
 import com.serotonin.bacnet4j.util.RequestUtils;
 import iot.technology.bacnet.util.BACnetUtil;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 public class BacnetYabeReadTest {
 
     public static void main(String[] args) throws BACnetException {
-        IpNetwork ipNetwork = BACnetUtil.initIpNetwork("172.30.176.1", 47808);
+        IpNetwork ipNetwork = BACnetUtil.initIpNetwork("172.20.16.1", 47808);
         LocalDevice localDevice = BACnetUtil.initLocalDevice(123, ipNetwork);
 
-        RemoteDevice remoteDevice = localDevice.getRemoteDeviceBlocking(2130082);
+        try {
+            RemoteDevice remoteDevice = localDevice.getRemoteDeviceBlocking(3309642);
 
-        List<ObjectIdentifier> objectList = RequestUtils.getObjectList(localDevice, remoteDevice).getValues();
-        objectList.forEach(objectIdentifier -> {
-            log.info("objectIdentifier: {}", objectIdentifier);
-        });
+            log.info("modelName: {}", remoteDevice.getDeviceProperty(PropertyIdentifier.modelName));
+            log.info("analogInput2: {}", RequestUtils.readProperty(localDevice, remoteDevice,
+                            new ObjectIdentifier(ObjectType.analogInput, 2),
+                            PropertyIdentifier.presentValue, null));
+
+            List<ObjectIdentifier> objectList = RequestUtils.getObjectList(localDevice, remoteDevice).getValues();
+            objectList.forEach(objectIdentifier -> {
+                log.info("objectIdentifier: {}", objectIdentifier);
+            });
+            ObjectIdentifier oid = new ObjectIdentifier(ObjectType.analogInput, 0);
+            ObjectIdentifier oid1 = new ObjectIdentifier(ObjectType.analogInput, 1);
+            ObjectIdentifier oid2 = new ObjectIdentifier(ObjectType.analogInput, 2);
+
+            //获取指定的presentValue
+            PropertyValues pvs = RequestUtils.readOidPresentValues(localDevice, remoteDevice, Arrays.asList(oid,oid1,oid2), new ReadListener(){
+                @Override
+                public boolean progress(double progress, int deviceId,
+                                        ObjectIdentifier oid, PropertyIdentifier pid,
+                                        UnsignedInteger pin, Encodable value) {
+                    System.out.println("========");
+                    System.out.println("progress=" + progress);
+                    System.out.println("deviceId=" + deviceId);
+                    System.out.println("oid="+oid.toString());
+                    System.out.println("pid="+pid.toString());
+                    System.out.println("UnsignedInteger="+pin);
+                    System.out.println("value="+value.toString() + "  getClass =" +value.getClass());
+                    return false;
+                }
+
+            });
+            Thread.sleep(3000);
+            System.out.println("analogInput:0 == " + pvs.get(oid, PropertyIdentifier.presentValue));
+            //获取指定的presentValue
+            PropertyValues pvs2 = RequestUtils.readOidPresentValues(localDevice, remoteDevice, Arrays.asList(oid,oid1,oid2),null);
+            System.out.println("analogInput:1 == " + pvs2.get(oid1, PropertyIdentifier.presentValue));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            localDevice.terminate();
+        }
     }
 }
